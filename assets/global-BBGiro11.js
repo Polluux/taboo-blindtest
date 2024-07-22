3840b93d7cf0e316e38d73986afcd1742a1c385d
Alexandre BOUDINE <alexandre.boudine@consertotech.pro>
2024-07-22 15:34:58 +0200

    change base url

---
+++ assets/global-BBGiro11.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index-Dj4WIzNo.js";const e=o("global",{state:()=>({counter:0}),actions:{}});export{e as u};
--- assets/global-CfRPvXaN.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-A3YgaWnw.js";const e=o("global",{state:()=>({counter:0}),actions:{}});export{e as u};
